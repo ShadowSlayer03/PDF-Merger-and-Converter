41a7d2de14a61e6a5ea0ccfc0fc40c067d55dd5f
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-11-23 20:32:38 +0530

    Update mergerFunc.js

--- mergerFunc.js
+++ mergerFunc.js
@@ -1,16 +1,23 @@
-import PDFMerger from 'pdf-merger-js'
+import PDFMerger from 'pdf-merger-js';
 
 var merger = new PDFMerger();
 
-const mergePDFs = async (filePaths,length,time) => {
-  for(let i=0;i<length;i++){
-        await merger.add(filePaths[i]);
-  }   
-    await merger.save(`public/merged/${time}_merged.pdf`); //save under given name and reset the internal document
-  
-  // Export the merged PDF as a nodejs Buffer
-  // const mergedPdfBuffer = await merger.saveAsBuffer();
-  // fs.writeSync('merged.pdf', mergedPdfBuffer);
-}
+const mergePDFs = async (filePaths, length, time) => {
+  try {
+    for (let i = 0; i < length; i++) {
+      await merger.add(filePaths[i]);
+    }
+    
+    await merger.save(`public/merged/${time}_merged.pdf`); // save under given name and reset the internal document
+    
+    // Export the merged PDF as a nodejs Buffer
+    // const mergedPdfBuffer = await merger.saveAsBuffer();
+    // fs.writeSync('merged.pdf', mergedPdfBuffer);
+  } catch (error) {
+    console.error('Error merging PDFs:', error);
+    // Handle the error or throw it again if you want to propagate it
+    throw error;
+  }
+};
 
 export { mergePDFs };
